2fa206ee591536ab92eaf503854174bf63b99696
fredzampier <fredzampier@gmail.com>
2024-08-21 15:59:14 -0300

    diagrama de contexto
    diagrama de contexto

--- src/todoz.api/obj/Debug/net7.0/todoz.api.AssemblyInfo.cs
+++ src/todoz.api/obj/Debug/net7.0/todoz.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todoz.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fe2ee1c43d5628be7f7384ae5de6ee68da61c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9dcdcaa1304069a49e9d351e6a943379527b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("todoz.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todoz.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
