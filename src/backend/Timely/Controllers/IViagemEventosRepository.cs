c2eed9b3bb235b8dd99294ff120298bbee5c1aec
Lizandra Ruiz <71288985+lizandraRuiz@users.noreply.github.com>
2024-10-01 20:51:49 -0300

    Add files via upload

---
+++ src/backend/Timely/Controllers/IViagemEventosRepository.cs
@@ -0,0 +1,13 @@
+﻿using Timely.Models;
+
+namespace Timely.Controllers
+{
+    public interface IViagemEventosRepository
+    {
+        void Add(ViagemEventos viagemEventos);
+        ViagemEventos? Get(int id);
+        void Update(ViagemEventos viagemEventos);
+        void Dispose();
+        void Save();
+    }
+}
---
+++ src/backend/Timely/Controllers/ViagemEventosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timely.Models;
+
+namespace Timely.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ViagemEventosController : ControllerBase
+    {
+        private readonly IViagemEventosRepository _viagemEventosRepository;
+
+        public ViagemEventosController(IViagemEventosRepository viagemEventosRepository)
+        {
+            _viagemEventosRepository = viagemEventosRepository;
+        }
+
+        [HttpPost]
+        public IActionResult Criar_ViagemEvento(ViagemEventos viagemEvento)
+        {
+            _viagemEventosRepository.Add(viagemEvento);
+            _viagemEventosRepository.Save();
+            return CreatedAtAction(nameof(Get), new { id = viagemEvento.ViagemEventosId }, viagemEvento);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ViagemEventos> Get(int id)
+        {
+            var viagemEvento = _viagemEventosRepository.Get(id);
+            if (viagemEvento == null)
+                return NotFound();
+            return Ok(viagemEvento);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, ViagemEventos viagemEvento)
+        {
+            if (id != viagemEvento.ViagemEventosId)
+                return BadRequest();
+
+            var existingViagemEvento = _viagemEventosRepository.Get(id);
+            if (existingViagemEvento is null)
+                return NotFound();
+
+            _viagemEventosRepository.Update(viagemEvento);
+            _viagemEventosRepository.Save();
+
+            return NoContent();
+        }
+    }
+}
