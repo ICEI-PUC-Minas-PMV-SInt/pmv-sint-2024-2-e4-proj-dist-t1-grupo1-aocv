b9d68390d0c8f22ccc1d729b0527cdeae72d5935
Arthur Viotti <131665533+arthurviotti@users.noreply.github.com>
2024-10-01 21:48:13 -0300

    Add files via upload

---
+++ src/backend/Timely/Controllers/ITarefaRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Timely.Repositories
+{
+    public interface ITarefaRepository : IDisposable
+    {
+        Tarefa Get(int id);
+        IEnumerable<Tarefa> GetAll();
+        void Add(Tarefa tarefa);
+        void Update(Tarefa tarefa);
+        void Delete(int id);
+    }
+}
