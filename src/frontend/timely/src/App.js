f51bf4efd77131f5b7227a67ffb9d861ed9cccbe
mailsonpuc <mailson.costa@proton.me>
2024-10-16 17:05:32 -0300

    componet esqueceu senha

--- src/frontend/timely/src/App.js
+++ src/frontend/timely/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import EsqueceuSenha from './components/EsqueceuSenha';
 
 function App() {
   return (
-    <div className="App">
+    <>
+     {/* 
+       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,8 +20,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+     
+        </header>
+           </div>
+     */}
+
+  
+        
+        <EsqueceuSenha/>
+    </>
   );
 }
 
